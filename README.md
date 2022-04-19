7f48b7fcc05fa731bc29e218784df6785f83093d
unknown <anders.sjunnesson@gmail.com>
2022-04-19 14:50:47 +0200

    Hello world

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Nodejs-intro
+
+# Initiera ett nytt Node.js projekt med npm init
+`npm init`
+
+Se till att skapa en fil som motsvaras av namnet - entry point - den fil som startar webbservern, ex:
+server.js
+
+Se dokumentation i server.js
+
+Starta server med kommando:
+`node server.js`
+
+Vid felmeddelande så beror det på att ES6 använder moduler
+- lägg till följande i package.json
+"type": "module",
---
+++ server.js
@@ -0,0 +1,22 @@
+// use core module http
+// commonJS || ES6 modules : require || import
+import http from 'http';
+
+// set port number
+const port = 3000;
+
+// create server
+const server = http.createServer((req, res) => {
+    console.log(req, res);
+
+    // response
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello world');
+    res.end();
+});
+
+// listen to requests
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
